e93f973acaf1137983b65c3e7a0716bd563137dc
Schnablette <anntschnabel@gmail.com>
2022-08-24 22:16:54 -0400

    polished and copy added

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,68 +1,150 @@
-import React from 'react';
+import React from "react";
 
-function SModal({closeModal}) {
+function SModal({ closeModal }) {
   return (
-    <div className='absolute z-0 inset-0 flex justify-center items-center' id='modalBackground'>
-      <div className='md:w-1/2 md:h-auto sm:w-3/4 sm:h-auto flex-column bg-purple-600 rounded-xl shadow-lg shadow-gray-400' id='modalContainer'>
-        <div className='w-full h-8 flex justify-end'>
-          <button className='px-4 py-2 text-2xl text-black bg-none shadow-none items-start justify-end' onClick={() => closeModal(false)}> X </button>
-        </div>
-        <div className='py-8 text-center' id='title'>
-          <h1 className='text-3xl'>DOer Supporter</h1>
-        </div>
-        <div className=''>
-          <p>Please embed here for minting DOer Supporter NFT!</p>
-        </div>
-        <div className='w-full h-12 flex justify-center items-center' id='footer'>
-          <button>Mint!</button>
+    <>
+      <div className="fixed z-50 inset-0 bg-purple-700 opacity-80" />
+      <div
+        className="fixed z-50 inset-0 flex justify-center items-center"
+        id="modalBackground"
+      >
+        <div
+          className="w-3/4 md:w-1/2 flex-column bg-purple-900 rounded-xl shadow-lg shadow-gray-400 pb-10"
+          id="modalContainer"
+        >
+          <div className="w-full h-8 flex justify-end">
+            <button
+              className="px-4 py-4 text-2xl hover:bg items-start justify-end"
+              onClick={() => closeModal(false)}
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="white"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
+              </svg>
+            </button>
+          </div>
+          <div className="py-8 text-center" id="title">
+            <h1 className="text-3xl">DOER Supporter</h1>
+          </div>
+          <div className="">{/* Embed HERE */}</div>
+          <div
+            className="w-full h-12 flex justify-center items-center"
+            id="footer"
+          >
+            <button
+              className="bg-purple-500 text-white rounded-md px-8 py-2 mt-2 text-base font-bold hover:bg-purple-200 active:bg-purple-300 uppercase"
+              onClick={() => closeModal(false)}
+            >
+              Mint
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
-
-function CModal({closeModal}) {
+function CModal({ closeModal }) {
   return (
-    <div className='absolute z-0 inset-0 flex justify-center items-center' id='modalBackground'>
-      <div className='md:w-1/2 md:h-auto sm:w-3/4 sm:h-auto flex-column bg-purple-600 rounded-xl shadow-lg shadow-gray-400' id='modalContainer'>
-        <div className='w-full h-8 flex justify-end'>
-          <button className='px-4 py-2 text-2xl text-black bg-none shadow-none items-start justify-end' onClick={() => closeModal(false)}> X </button>
-        </div>
-        <div className='py-8 text-center' id='title'>
-          <h1 className='text-3xl'>DOer Champion</h1>
-        </div>
-        <div className=''>
-          <p>Please embed here for minting DOer Supporter NFT!</p>
-        </div>
-        <div className='w-full h-12 flex justify-center items-center' id='footer'>
-          <button>Mint!</button>
+    <>
+      <div className="fixed z-50 inset-0 bg-purple-700 opacity-80" />
+      <div
+        className="fixed z-50 inset-0 flex justify-center items-center"
+        id="modalBackground"
+      >
+        <div
+          className="w-3/4 md:w-1/2 flex-column bg-purple-900 rounded-xl shadow-lg shadow-gray-400 pb-10"
+          id="modalContainer"
+        >
+          <div className="w-full h-8 flex justify-end">
+            <button
+              className="px-4 py-4 text-2xl hover:bg items-start justify-end"
+              onClick={() => closeModal(false)}
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="white"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
+              </svg>
+            </button>
+          </div>
+          <div className="py-8 text-center" id="title">
+            <h1 className="text-3xl">DOER Champion</h1>
+          </div>
+          <div className="">{/* Embed HERE */}</div>
+          <div
+            className="w-full h-12 flex justify-center items-center"
+            id="footer"
+          >
+            <button
+              className="bg-purple-500 text-white rounded-md px-8 py-2 mt-2 text-base font-bold hover:bg-purple-200 active:bg-purple-300 uppercase"
+              onClick={() => closeModal(false)}
+            >
+              Mint
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-  }
-
-
-function LModal({closeModal}) {
-  return (
-    <div className='absolute z-0 inset-0 flex justify-center items-center' id='modalBackground'>
-      <div className='md:w-1/2 md:h-auto sm:w-3/4 sm:h-auto flex-column bg-purple-600 rounded-xl shadow-lg shadow-gray-400' id='modalContainer'>
-        <div className='w-full h-8 flex justify-end'>
-          <button className='px-4 py-2 text-2xl text-black bg-none shadow-none items-start justify-end' onClick={() => closeModal(false)}> X </button>
-        </div>
-        <div className='py-8 text-center' id='title'>
-          <h1 className='text-3xl'>DOer Legend</h1>
-        </div>
-        <div className=''>
-          <p>Please embed here for minting DOer Supporter NFT!</p>
-        </div>
-        <div className='w-full h-12 flex justify-center items-center' id='footer'>
-          <button>Mint!</button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
-export { SModal, CModal, LModal }
+function LModal({ closeModal }) {
+  return (
+    <>
+      <div className="fixed z-50 inset-0 bg-purple-700 opacity-80" />
+      <div
+        className="fixed z-50 inset-0 flex justify-center items-center"
+        id="modalBackground"
+      >
+        <div
+          className="w-3/4 md:w-1/2 flex-column bg-purple-900 rounded-xl shadow-lg shadow-gray-400 pb-10"
+          id="modalContainer"
+        >
+          <div className="w-full h-8 flex justify-end">
+            <button
+              className="px-4 py-4 text-2xl hover:bg items-start justify-end"
+              onClick={() => closeModal(false)}
+            >
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5"
+                fill="white"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"></path>
+              </svg>
+            </button>
+          </div>
+          <div className="py-8 text-center" id="title">
+            <h1 className="text-3xl">DOER Legend</h1>
+          </div>
+          <div className="">{/* Embed HERE */}</div>
+          <div
+            className="w-full h-12 flex justify-center items-center"
+            id="footer"
+          >
+            <button
+              className="bg-purple-500 text-white rounded-md px-8 py-2 mt-2 text-base font-bold hover:bg-purple-200 active:bg-purple-300 uppercase"
+              onClick={() => closeModal(false)}
+            >
+              Mint
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export { SModal, CModal, LModal };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import { SModal, CModal, LModal} from '../components/modules/Modal';
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { SModal, CModal, LModal } from "../components/Modal";
 
 export default function Home() {
   const [openSModal, setOpenSModal] = useState(false);
@@ -10,94 +10,177 @@ export default function Home() {
   const [openLModal, setOpenLModal] = useState(false);
 
   return (
-    <div>
+    <>
       <Head>
-        <title>Web3Acadmey DAO Donations</title> 
-        <meta name='description' content='Donation page for Web3 Acadmey DAO' />
-        <link rel='icon' href='/favicon.ico' />
+        <title>Web3Acadmey DAO Donations</title>
+        <meta name="description" content="Donation page for Web3 Acadmey DAO" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       {/* Header Section */}
-      <div className='h-auto w-screen'>
-          <div className='px-10 py-10 w-screen md:h-96 sm:h-screen bg-gradient-to-b from-purple-800 to-black justify-between md:flex sm:flex-column gap-16'>
-            <div className='justify-center items-center'>
-              <Image src='/../public/assets/imgs/h-logo.png' alt='/' width='300' height='150' />
-              <h3 className='sm:text-5xl md:text-3xl uppercase'>Become a W3A DAO Donor</h3>
-              <p className='pt-4 text-sm'>Tier donorship entitles you become a champion of Web3.</p>
-              <p className='text-sm p-2'>A donor supporting the evolution of the new age of technology.</p>
-            </div>
-            <div className='md:w-1/4 sm:w-full h-full flex-column justify-center items-center'>
-              <Image src='/../public/assets/imgs/logos.png' alt='/' width='200' height='100' />
-              <p className='text-3xl text-gray-40 text-center'>#AreYouReady...?</p>
-            </div>
+      <div id="stars" className="z-0"></div>
+      <div id="stars2" className="z-0"></div>
+      <div id="stars3" className="z-0"></div>
+      <div className="px-10 py-10 md:px-20 bg-gradient-to-b from-purple-800 to-black ">
+        <Image
+          src="/../public/assets/imgs/h-logo.png"
+          alt="Web3 Academy DAO logo"
+          width="150"
+          height="90"
+        />
+        <div className="justify-between lg:flex sm:flex-column gap-16">
+          <div className="justify-center items-center">
+            <h1 className="mt-20 text-center md:text-left leading-tight">
+              <span className="font-light">Become a W3A DAO </span>
+              <br />
+              Genesis Donator
+            </h1>
+            <p className="mt-10 max-w-2xl text-center mb-20 md:text-left">
+              Web3 Academy DAO is a tokenized community on a mission to create
+              resources, products and services to better support the wave of
+              entrepreneurs, businesses and creators moving into Web3 over the
+              coming decade.
+            </p>
           </div>
-      </div>
-
-      {/* Hero Section */}
-      <div className='w-screen h-auto flex justify-center items-center text-center'>
-      <div className='py-20'>
-          <h1 className='py-20'>What is Web3 Academy DAO?</h1>
-          <h2 className='py-2'>Web3 Academy DAO is a community, formed from the Web3</h2>
-          <h2 className='py-2'>Academy Discord.  It originated from a common interest in</h2>
-          <h2 className='py-2'>learning and growing the web3 ecosystem.</h2>
+          <div className="md:w-1/2 flex-column justify-center items-center">
+            <Image
+              src="/../public/assets/imgs/logos.png"
+              alt="DOER Icons"
+              width="1000"
+              height="500"
+            />
+            <p className="text-3xl font-light text-gray-40 text-center mt-20">
+              #AreYouReady?
+            </p>
+          </div>
         </div>
       </div>
 
       {/* NFT Tier Section */}
-      <div className='w-full h-screen py-8 flex-column justify-center items-center static'>
-        <div className='w-full text-center'>
-            <h1 className='md:text-5xl sm:text-4xl'>Tier Donor Options</h1>
-        </div>
-        <div className='w-full h-1/3 flex justify-center items-center'>
-            <div className='w-96'>
-                <h3 className='sm:text-xl md:text-lg text-indigo-700 uppercase'>tier 1</h3>
-                <h1 className='text-3xl'>DOer Supporter</h1>
-                <p className='py-4'>Lorem Ipsum is ismply dummy text of the printing and typsetting industry.  Lorem Ipsum has been the industry's standard dfdfd</p>
-                <button type='button' className='bg-indigo-400 text-white rounded-md px-8 py-2 text-base font-medium hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-600 shadow-none uppercase' onClick={() => setOpenSModal(true)}>Mint!</button>
-                {openSModal && <SModal closeModal={setOpenSModal} />}
-            </div>
-            <div className='w-56'>
-                <Image src="/../public/assets/imgs/Tier-1.png" alt="/" width="400" height="700" />
-            </div>
+      <h2 className="md:text-5xl sm:text-4xl max-w-xl text-center mx-auto mt-20 ">
+        <span className="font-light">Web3 Academy DAO </span>
+        <br />
+        Genesis Donators Collection
+      </h2>
+      <div className="mt-10 flex-column justify-center items-center mb-20 px-5">
+        <div className="w-full h-1/3 flex justify-center items-center mb-20">
+          <div className="w-96">
+            <h3 className="sm:text-xl md:text-lg text-purple-100 uppercase">
+              Tier 1
+            </h3>
+            <h2 className="text-3xl">DOER Supporter</h2>
+            <p className="py-4">
+              A DOER Supporter holder shows that you are here for the Mission!
+              Those who mint this NFT will forever be remembered as a supporter
+              of the genesis of Web3 Academy DAO.
+            </p>
+            <button
+              disabled={openSModal || openCModal || openLModal}
+              className="bg-purple-500 text-white rounded-md px-8 py-2 mt-2 text-base font-bold hover:bg-purple-200 active:bg-purple-300 uppercase"
+              onClick={() => setOpenSModal(true)}
+            >
+              Mint
+            </button>
+            {openSModal && <SModal closeModal={setOpenSModal} />}
+          </div>
+          <div className="w-56">
+            <Image
+              src="/../public/assets/imgs/Tier-1.png"
+              alt="/"
+              width="400"
+              height="450"
+            />
+          </div>
         </div>
 
-        <div className='w-full h-1/3 flex justify-center items-center'>
-            <div className='w-56'>
-                <Image src="/../public/assets/imgs/Tier-2.png" alt="/" width="400" height="700" />
-            </div>
-            <div className='w-96'>
-                <h3 className='text-lg text-indigo-700 uppercase'>Tier 2</h3>
-                <h1 className='text-3xl'>DOer Champion</h1>
-                <p className='py-4'>Lorem Ipsum is ismply dummy text of the printing and typsetting industry.  Lorem Ipsum has been the industry's standard dfdfd</p>
-                <button type='button' className='bg-indigo-400 text-white rounded-md px-8 py-2 text-base font-medium hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-600 shadow-none uppercase' onClick={() => setOpenCModal(true)}>Mint!</button>
-                {openCModal && <CModal closeModal={setOpenCModal} />}
-            </div>
+        <div className="w-full h-1/3 flex justify-center items-center mb-20">
+          <div className="w-56">
+            <Image
+              src="/../public/assets/imgs/Tier-2.png"
+              alt="/"
+              width="400"
+              height="450"
+            />
+          </div>
+          <div className="w-96">
+            <h3 className="sm:text-xl md:text-lg text-purple-100 uppercase">
+              Tier 1
+            </h3>
+            <h2 className="text-3xl">DOER Champion</h2>
+            <p className="py-4">
+              A DOER Champion holder shows that you are here for the mission and
+              committed to the cause! Those who mint this NFT will forever be
+              remembered as a champion of the genesis of Web3 Academy DAO.
+            </p>
+            <button
+              disabled={openSModal || openCModal || openLModal}
+              className="bg-purple-500 text-white rounded-md px-8 py-2 mt-2 text-base font-bold hover:bg-purple-200 active:bg-purple-300 uppercase"
+              onClick={() => setOpenCModal(true)}
+            >
+              Mint
+            </button>
+            {openCModal && <CModal closeModal={setOpenCModal} />}
+          </div>
         </div>
 
-        <div className='w-full h-1/3 flex justify-center items-center'>
-            <div className='w-96'>
-                <h3 className='text-lg text-indigo-700 uppercase'>tier 1</h3>
-                <h1 className='text-3xl'>DOer Legend</h1>
-                <p className='py-4'>Lorem Ipsum is ismply dummy text of the printing and typsetting industry.  Lorem Ipsum has been the industry's standard dfdfd</p>
-                <button type='button' className='bg-indigo-400 text-white rounded-md px-8 py-2 text-base font-medium hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-600 shadow-none uppercase' onClick={() => setOpenLModal(true)}>Mint!</button>
-                {openLModal && <LModal closeModal={setOpenLModal} />}
-            </div>
-            <div className='w-56'>
-                <Image src="/../public/assets/imgs/Tier-3.png" alt="/" width="400" height="700" />
-            </div>
+        <div className="w-full h-1/3 flex justify-center items-center">
+          <div className="w-96">
+            <h3 className="sm:text-xl md:text-lg text-purple-100 uppercase">
+              Tier 1
+            </h3>
+            <h2 className="text-3xl">DOER Legend</h2>
+            <p className="py-4">
+              A DOER Legend holder shows that you are here for the mission and
+              putting your money where your mouth is! Those who mint this NFT
+              will forever be remembered as a legend of the genesis of Web3
+              Academy DAO.
+            </p>
+            <button
+              disabled={openSModal || openCModal || openLModal}
+              className="bg-purple-500 text-white rounded-md px-8 py-2 mt-2 text-base font-bold hover:bg-purple-200 active:bg-purple-300 uppercase"
+              onClick={() => setOpenLModal(true)}
+            >
+              Mint
+            </button>
+            {openLModal && <LModal closeModal={setOpenLModal} />}
+          </div>
+          <div className="w-56">
+            <Image
+              src="/../public/assets/imgs/Tier-3.png"
+              alt="/"
+              width="400"
+              height="450"
+            />
+          </div>
         </div>
       </div>
       {/* Footer Section */}
-      <div className='w-screen h-[500px] pt-48 bg-gradient-to-b from-black to-indigo-600'>
-          <div className=''>
-            <div className='text-center'>
-              <h1 className='uppercase text-4xl'>Web3 academy dao</h1>
-                <p className='text-sm pt-2 pb-8'>Community of like minded individuals that gather to form the Web3 Academy DAO.</p>
-                <button className='w-64 h-12 uppercase shadow-none text-xl'>join our discord</button>
-            </div>
+      <div className="bg-gradient-to-t from-purple-800 to-black pb-24 pt-36 ">
+        <div className="text-center ">
+          <Image
+            src="/../public/assets/imgs/h-logo.png"
+            alt="Web3 Academy DAO logo"
+            width="150"
+            height="90"
+          />
         </div>
       </div>
-
-    </div>
-  )
+      {/* <div className="w-screen h-[500px] pt-48 bg-gradient-to-b from-black to-purple-800">
+        <div className="">
+          <div className="text-center">
+            <h1 className="uppercase text-4xl">Web3 academy dao</h1>
+            <p className="text-sm pt-2 pb-8">
+              A DOER Legend holder shows that you are here for the mission and
+              putting your money where your mouth is! Those who mint this NFT
+              will forever be remembered as a legend of the genesis of Web3
+              Academy DAO.
+            </p>
+            <button disabled={openSModal || openCModal  || openLModal } className="bg-purple-500 hover:bg-purple-200 active:bg-purple-300  text-white rounded-md px-8 py-2 w-64 h-12 uppercase shadow-none text-xl">
+              join our discord
+            </button>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
 }
